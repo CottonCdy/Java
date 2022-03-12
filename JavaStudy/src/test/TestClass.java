cadb137d9a4caa53a3ca02d42687e2ef70c3a458
Code <taste397@naver.com>
2022-03-12 21:40:58 +0900

    commit

---
+++ JavaStudy/src/test/TestClass.java
@@ -0,0 +1,14 @@
+package test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Eclipse Test");
+		System.out.println("Eclipse Test");
+		System.out.println("git test");
+		System.out.println("git test2");
+		System.out.println("git test3");
+		System.out.println("git test4");
+	}
+
+}
