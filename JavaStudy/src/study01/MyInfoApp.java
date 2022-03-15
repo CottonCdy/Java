4796024d8135e4f2e92e26bc6354b565b572bbc6
Code <taste397@naver.com>
2022-03-15 21:37:06 +0900

    study01 commit

---
+++ JavaStudy/src/study01/MyInfoApp.java
@@ -0,0 +1,10 @@
+package study01;
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		System.out.println("Kitae Hawng");
+		System.out.println("20 years old");
+		System.out.println("Department of Computer Engineering");
+
+	}
+}
